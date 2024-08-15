804a5a2bf5e8326fe934090aae7691f0ddfe884c
vikySeeker <vikyseeker@gmail.com>
2024-08-15 17:39:18 +0530

    Update codebase with db code update makefile

---
+++ db/db.h
@@ -0,0 +1,20 @@
+//include gaurd
+#ifndef CREDS_DB_H
+#define CREDS_DB_H
+
+#include<sqlite3.h>
+// struct used to store credentials that are handled by this program
+typedef struct creds {
+	char *username;
+	char *password;
+	char *tag;
+} creds;
+
+void free_db_stuff(char *err_msg);
+sqlite3* getDBHandle();
+int save_creds (int argc, char **argv);
+int get_creds_id(char *search_value, sqlite3 *db);
+int get_creds (char *search_value);
+int delete_creds (char *search_value);
+
+#endif
--- makefile
+++ makefile
@@ -1,9 +1,16 @@
-CFLAGS = -lsqlite3
+LFLAGS = -lsqlite3
+
+default: build
 
 build:
-	@echo "compiling the main file..."
-	gcc -o c-creds main.c $(CFLAGS)
+	@echo "building db object file"
+	gcc -c db/db.c
+	@echo "building main object file"
+	gcc -c main.c
+	@echo "building c-creds executable file"
+	gcc -o c-creds main.o db.o $(LFLAGS)
+	@echo "\nc-creds built successfully!!"
 
 clean:
 	@echo "removing everything but the source files..."
-	rm c-creds
+	rm c-creds *.o
